18fa3b87e7cb167b4f0e399df572b9350819ddf1
Glen Maddern <glenmaddern@gmail.com>
2013-04-27 15:24:10 +1000

    Got bespoke working!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,8 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('webDirectionsTalkApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  console.log('ok');
+
+  bespoke.horizontal.from('article');
+})();
+
