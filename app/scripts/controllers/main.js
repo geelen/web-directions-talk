dd83ce5ac39a3cdb524737aad2aec7a5f12f1ab0
Glen Maddern <glenmaddern@gmail.com>
2013-04-27 12:47:27 +1000

    Generated Angular app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('webDirectionsTalkApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
