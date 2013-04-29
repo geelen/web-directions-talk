c376fc8d8ffee123b91f590aca00264b77d61e07
Glen Maddern <glenmaddern@gmail.com>
2013-04-29 17:36:44 +1000

    leaving

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,16 +9,18 @@
 
   var i, image, j, matches, rules, sheet;
 
-  for (i = 0; i < document.styleSheets.length; ++i) {
-    sheet = document.styleSheets[i];
-    if (sheet.rules) {
-      for (j = 0; j < sheet.rules.length; ++j) {
-        rules = sheet.rules[j];
-        if (rules.style && rules.style.backgroundImage) {
-          matches = rules.style.backgroundImage.match(/url\((.*)\)/);
-          if (matches) {
-            image = new Image;
-            image.src = matches[1];
+  if (document.styleSheets) {
+    for (i = 0; i < document.styleSheets.length; ++i) {
+      sheet = document.styleSheets[i];
+      if (sheet.rules) {
+        for (j = 0; j < sheet.rules.length; ++j) {
+          rules = sheet.rules[j];
+          if (rules.style && rules.style.backgroundImage) {
+            matches = rules.style.backgroundImage.match(/url\((.*)\)/);
+            if (matches) {
+              image = new Image;
+              image.src = matches[1];
+            }
           }
         }
       }
